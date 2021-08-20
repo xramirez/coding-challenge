c89886e662d7906f3624ace987809ce89accc5a6
xramirez <xeramirez1997@gmail.com>
2021-08-20 16:14:30 -0500

    Completed Problem 3
    Created a regular expression that checks if a string has "happy" g's

--- exercises/gHappy.js
+++ exercises/gHappy.js
@@ -1,13 +1,31 @@
-module.exports.run = function(str) {
-/*
-A lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
-Return true if all the g's in the given string are happy, otherwise return false.
+module.exports.run = function (str) {
+	/*
+	A lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
+	Return true if all the g's in the given string are happy, otherwise return false.
+	
+		Example:
+			function("xxggxx") => true
+			function("xxgxx") => false
+	
+			Write your code below the comment.
+	*/
 
-	Example:
-		function("xxggxx") => true
-		function("xxgxx") => false
+	//Before anything, this function will check if the string even has g's
+	//A string can't have happy g's if there's no g's
 
-        Write your code below the comment.
-*/
+	//Rather than checking to make sure that every single instance of g has multiples next to it, why not check to see if there's just one g that is alone?
+	//We can set up a regular expression that reads through a string and will simply return false if it detects a *single* g that is not repeating
+	//Specifically, it matches the position in a string of a g that has no g  before it, and no repeating g's after it
+	//If no pattern like that is found, it returns -1, meaning no position has a lone g, thus meaning the string is "happy"
 
+	if (str.search("g") != -1) {
+		let readThrough = str.search(/(?<!g)g(?!g+)/);
+		//console.log(readThrough == -1);
+		return (readThrough == -1);
+	}
+	else
+		return false;
+	//Alternatively, what if there are no g's?
 };
+
+module.exports.run("xxgxx");
